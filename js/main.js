51c4f2364989bef23277ac2f7f635b4477205653
Matt Mathias <matt.mathias@gyro.com>
2020-01-30 16:33:42 -0700

    updates

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 'use strict';
 
-var videoElement = document.querySelector('video');
-var videoSelect = document.querySelector('select#videoSource');
 var library = document.querySelector('input#library');
 var camera = document.querySelector('input#camera');
+var buttons = document.querySelector('#buttons');
+var loader = document.querySelector('.loader');
 
 var s3Region = "us-east-1";
 var s3BucketName = "gyro-corning-video-test";
@@ -19,58 +19,8 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 library.addEventListener('change', uploadVideo);
 camera.addEventListener('change', uploadVideo);
 
-videoSelect.onchange = getStream;
 
-getStream().then(getDevices).then(gotDevices);
 
-function getDevices() {
-  // AFAICT in Safari this only gets default devices until gUM is called :/
-  return navigator.mediaDevices.enumerateDevices();
-}
-
-function gotDevices(deviceInfos) {
-  window.deviceInfos = deviceInfos; // make available to console
-  console.log('Available input and output devices:', deviceInfos);
-  for (const deviceInfo of deviceInfos) {
-    const option = document.createElement('option');
-    option.value = deviceInfo.deviceId;
-    if (deviceInfo.kind === 'audioinput') {
-    //   option.text = deviceInfo.label || `Microphone ${audioSelect.length + 1}`;
-    //   audioSelect.appendChild(option);
-    } else if (deviceInfo.kind === 'videoinput') {
-      option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
-      videoSelect.appendChild(option);
-    }
-  }
-}
-
-function getStream() {
-  if (window.stream) {
-    window.stream.getTracks().forEach(track => {
-      track.stop();
-    });
-  }
-  //const audioSource = audioSelect.value;
-  const videoSource = videoSelect.value;
-  const constraints = {
-    //audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
-    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
-  };
-  return navigator.mediaDevices.getUserMedia(constraints).
-    then(gotStream).catch(handleError);
-}
-
-function gotStream(stream) {
-  window.stream = stream; // make stream available to console
-  //audioSelect.selectedIndex = [...audioSelect.options].findIndex(option => option.text === stream.getAudioTracks()[0].label);
-  videoSelect.selectedIndex = [...videoSelect.options].
-    findIndex(option => option.text === stream.getVideoTracks()[0].label);
-  videoElement.srcObject = stream;
-}
-
-function handleError(error) {
-  console.error('Error: ', error);
-}
 
 function uploadVideo(event) {
   var files = event.target.files;
@@ -92,14 +42,20 @@ function uploadVideo(event) {
     }
   });
 
+  buttons.classList.add('hidden')
+  loader.classList.remove('hidden')
   var promise = upload.promise();
 
   promise.then(
     function(data) {
-      alert("Successfully uploaded video.");
+      buttons.classList.remove('hidden')
+      loader.classList.add('hidden')
+      alert("Successfully uploaded video.");      
     },
     function(err) {
       console.log(err)
+      buttons.classList.remove('hidden')
+      loader.classList.add('hidden')
       return alert("There was an error uploading your video: ", err.message);
     }
   );
